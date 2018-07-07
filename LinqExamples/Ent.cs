850745382f2fa12c00b05948dbf4cd8b32ea22d2
narminn <nasirovanarmin96@gmail.com>
2018-07-07 15:09:39 +0400

    lab task

---
+++ LinqExamples/Ent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExamples
+{
+    class Entity
+    {
+        public static List<Products> products = new List<Products>()
+        {
+            new Products{ProoductName="Pencil",Price=2,StockCount=100},
+            new Products{ProoductName="Pen",Price=1,StockCount=140},
+            new Products{ProoductName="Book",Price=5,StockCount=24},
+            new Products{ProoductName="Notebook",Price=4,StockCount=68},
+            new Products{ProoductName="Ruler",Price=2,StockCount=120},
+            new Products{ProoductName="Toy",Price=12,StockCount=32},
+            new Products{ProoductName="Pen",Price=3,StockCount=145},
+            new Products{ProoductName="Book",Price=10,StockCount=90},
+            new Products{ProoductName="Pen",Price=1,StockCount=125},
+            new Products{ProoductName="Toy",Price=15,StockCount=77},
+            new Products{ProoductName="Ruler",Price=6,StockCount=40},
+            new Products{ProoductName="Book",Price=17,StockCount=16},
+
+
+
+        };
+    }
+}
---
+++ LinqExamples/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace LinqExamples
+{
+    class Program
+    {
+        //EntityClass EntityClass = new EntityClass();
+        static void Main(string[] args)
+        {
+           
+
+            string[] names = { "Narmin", "Sekine", "Elvin", "Emin", "Naib", "Zohrali", "Fazil Abi", "Suleyman", "Zamin", "Nurlan" };
+
+            int[] numbers = { 5, 7, 2, 6, 12, 25, 17, 9, 26, 33, 41 };
+            //2.Arraylere aid olan queryler
+
+            //2.1 Eyni herfle baslayan adlari qruplasdirib hansi herfle basladigini qeyd ederek ekrana cixardin
+            //Meselen N herfi ile baslayan adlar:
+            //Narmin,Nurlan,Naib...
+
+
+            //2.2.Numbers arrayi icerisinde 5e bolunende eyni qaligi alinan ededleri qruplasdirib qaligini qeyd ederek ekrana cixardin
+            //Meselen 5e bolunerken qaligi 1 olan ededler 6,26,41
+
+            //2.3.Numbers arrayi icersinde cut ededlerden en kiciyinin 3 qatini ekrana cixardin
+
+
+
+            int[] nums = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            string[] str = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            //2.4.nums arrayindeki ededleri strdeki adlarina uygun ekrana yazdirin
+            //meselen birinci 5 verilib five ciarsin sonra four one ve s....
+
+
+
+            int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+            int[] numbersB = { 1, 3, 5, 7, 8 };
+
+            //2.5 numbersA arrayindeki reqemlerin numbersB arrayindeki ededlerle muqayisesinden kicik olanlari ekrana cixarin
+            //Ekrana cixmalidir :
+            //0 kicikdir 1-den
+            //0 kicikdir 3-den
+            //0 kicikdir 5-den
+            //0 kicikdir 7-den
+            //0 kicikdir 8-den
+            //2 kicikdir 3-den .....
+
+            int[] ededler = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            //2.6 ededler arrayindeki reqemlerin duzgun indexde olub olmadigini yoxlayin
+            //Ekrana cixmalidir:
+            //5:false ...
+            //3:true...
+
+            string[] digits = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            //2.7 digits arrayindeki reqemlerden ikinci herfi 'i' olanlari sondan evvele dogru ekrana cixaran sorgu yazin
+
+            int[] numbersC = { 0, 2, 4, 5, 6, 8, 9 };
+            int[] numbersD = { 1, 3, 5, 7, 8 };
+
+            //2.8 numbersC ve numbersD arraylerindeki ortaq reqemleri ekrana cixaran sorgu yazin
+
+            string[] words = { "cherry", "apple", "blueberry" };
+
+            //2.9 words arrayindeki sozlerin uzunluqlarinin cemini tapin
+
+            int[] arr = { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            //2.10 arr arrayindeki ededlerin tekrarlanma sayini ekrana cixaran sorgu yazin
+            
+            string[] cities =
+            {
+                "ROME","LONDON","NAIROBI","CALIFORNIA","ZURICH","NEW DELHI","AMSTERDAM","ABU DHABI", "PARIS"
+            };
+
+            //2.11 ekrandan ilk ve son herf daxil olunacaq ilk herfle baslayib son herfle biten seherleri (cities arrayinin icinden)
+            //ekrana cixaran sorgu yazin.  
+
+
+        }
+        
+    }
+}
